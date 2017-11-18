13b3ba187734733177207587e7777ed6c3e92215
Greg Rose <gregrose628@gmail.com>
2017-11-18 01:30:39 -0500

    completed step 4 of react-marathon challenge

--- react-marathon/src/containers/App.js
+++ react-marathon/src/containers/App.js
@@ -9,11 +9,17 @@ class App extends React.Component {
 
   render() {
     let data = this.props.data;
+    let selectedPlaylist = data.playlists.find(playlist => {
+      return playlist.id == data.selectedPlaylistId;
+    });
+    let currentSongs = data.songs.filter(song => {
+      return selectedPlaylist.songs.includes(song.id);
+    });
 
     return (
       <ul className="App row">
         <PlaylistCollection playlists={data.playlists} selectedPlaylistId={data.selectedPlaylistId} />
-        <SongCollection songs={data.songs} />
+        <SongCollection songs={currentSongs} />
       </ul>
     );
   }
