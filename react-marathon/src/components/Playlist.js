b11a17f3f7739902f243959a0bc0d8d177f5b868
Greg Rose <gregrose628@gmail.com>
2017-11-18 01:34:08 -0500

    completed step 5 of react-marathon challenge

--- react-marathon/src/components/Playlist.js
+++ react-marathon/src/components/Playlist.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const Playlist = props => {
+  let handleClick = () => {
+    alert(`You've clicked ${props.name}`);
+  };
+
   return (
-    <li className={props.className}>
+    <li className={props.className} onClick={handleClick}>
       <h2>{props.name}</h2>
       <h3>{props.description}</h3>
     </li>
